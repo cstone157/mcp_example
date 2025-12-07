79b7ecc48e04a6dbfef803da3c9a8106cb5131c6
cstone157 <culver.stone57@gmail.com>
2025-12-06 21:32:12 -0800

    Nothing of note

--- delete.sh
+++ delete.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-## Delete the image registry
-helm delete image-registry
+# # V1 
+# ## Delete the image registry
+# helm delete image-registry
 
-## Delete the certificate issuer
-kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v1.3.0/cert-manager.yaml
-kubectl delete -f ./dev/cert-manager/ClusterIssuer.yaml
-kubectl delete namespace cert-manager
+# ## Delete the certificate issuer
+# kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v1.3.0/cert-manager.yaml
+# kubectl delete -f ./dev/cert-manager/ClusterIssuer.yaml
+# kubectl delete namespace cert-manager
+
+
+kubectl delete -f dev/cluster-issuer.yaml
+kubectl delete namespace cert-manager 
--- setup.sh
+++ setup.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-# Setup the certificate-manager (https://www.slingacademy.com/article/how-to-set-up-ssl-with-lets-encrypt-in-kubernetes/)
-kubectl create namespace cert-manager
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.0/cert-manager.yaml
-kubectl apply -f ./dev/cert-manager/ClusterIssuer.yaml
+# # V1 Setup
+# # Setup the certificate-manager (https://www.slingacademy.com/article/how-to-set-up-ssl-with-lets-encrypt-in-kubernetes/)
+# kubectl create namespace cert-manager
+# kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.0/cert-manager.yaml
+# kubectl apply -f ./dev/cert-manager/ClusterIssuer.yaml
 
-# Add the certificate manager
-helm install image-registry ./dev/image-registry/
+# # Add the certificate manager
+# helm install image-registry ./dev/image-registry/
+# # ===================================================
+
+
+
+## == Setup the Image Registry ==
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set installCRDs=true
+# Alternatively install using the yaml
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.4/cert-manager.yaml
+
+kubectl apply -f dev/cluster-issuer.yaml
+
+
+## == Setup the Certificate Manager ==
+# sudo apt install apache2-utils
+
