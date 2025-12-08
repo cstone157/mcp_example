273ec47014574787002575ca25c4e2fd0b734f51
cstone157 <culver.stone57@gmail.com>
2025-12-08 15:33:28 -0800

    Hate the registry

--- delete.sh
+++ delete.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Delete Registry
+kubectl delete namespace registry
+
+# Delete cert-manager
+kubectl delete -f dev/cert/letsencrypt-issuer.yaml
+kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.0/cert-manager.yaml
 
--- setup.sh
+++ setup.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
 
-# # V1 Setup
-# # Setup the certificate-manager (https://www.slingacademy.com/article/how-to-set-up-ssl-with-lets-encrypt-in-kubernetes/)
-# kubectl create namespace cert-manager
-# kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.0/cert-manager.yaml
-# kubectl apply -f ./dev/cert-manager/ClusterIssuer.yaml
+# Create a certificate manager
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.0/cert-manager.yaml
+sleep 10 # We need to wait a few seconds to ensure that that certificate-manager has actually deployed
+kubectl apply -f dev/cert/letsencrypt-issuer.yaml
 
-# # Add the certificate manager
-# helm install image-registry ./dev/image-registry/
-# # ===================================================
-
-
-
-## == Setup the Image Registry ==
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.13.3 --set installCRDs=true
-kubectl apply -f dev/cluster-issuer.yaml
-
-
-# Create a secret
-# sudo apt install apache2-utils
-# htpasswd -c -B -b auth myuser mypassword
-htpasswd -Bc auth admin
-kubectl create secret generic registry-auth-secret --from-file=htpasswd=auth --namespace default
-
-
-## == Setup the Certificate Manager ==
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-
-helm install private-registry bitnami/docker-registry -f bitnami-values.yaml --namespace default
+# Create a namespace for a local registry
+kubectl create namespace registry
+kubectl apply -f dev/registry/registry-certificate.yaml
+kubectl apply -f dev/registry/registry-pvc.yaml
+kubectl apply -f dev/registry/registry-deployment.yaml
+kubectl apply -f dev/registry/registry-service.yaml
+kubectl apply -f dev/registry/registry-ingress.yaml
